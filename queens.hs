4aaf93282a9b57c3a2d724a4e716a62973dcb895
ane <ane@iki.fi>
2011-09-08 13:46:43 +0300

    Add queens.hs

---
+++ queens.hs
@@ -0,0 +1,37 @@
+-- A rudimentary solver for the N queens problem
+-- (c) ane <ane@iki.fi> 2009
+-- (Assignment for Functional Programming 1 at JYU)
+
+module Main where
+
+nqueens :: Int -> [[Int]]
+nqueens 0 = [[]]
+nqueens n = foldl (addqueen n) [[]] [1..n]
+
+addqueen :: Int -> [[Int]] -> Int -> [[Int]]
+addqueen width prev_solutions index =
+  let newcolumn = [1 .. width] in
+  (concatMap
+      (\solution ->
+        map (: solution)
+          (filter (`validSet` solution) newcolumn)) prev_solutions)
+
+validSet :: (Num a, Enum a) => a -> [a] -> Bool
+validSet newcolumn solution = all (\(column, row) -> column /= newcolumn && column + row /= newcolumn && column - row /= newcolumn) (zip solution [1 ..])
+
+visualize ::  (Num a, Enum a) => [a] -> a -> [String]
+visualize sol width = map mapLine (mapSolution sol width)
+  where
+    mapLine = map (\a -> if a then 'Q' else '.')
+
+mapSolution :: (Num a, Enum a) => [a] -> a -> [[Bool]]
+mapSolution solution width = map columnVisual solution
+  where
+    columnVisual idx = [ x == idx | x <- [1 .. width]]
+
+formatBoard :: [[Int]] -> Int -> [String]
+formatBoard solutions width = map (unlines . (\blob -> ("  " ++ take width ['A' .. 'Z']) : blob)) (indexRow (map (`visualize` width) solutions))
+  where
+    indexRow = map (\sol -> map (\(idx, row) -> show idx ++ "|" ++ row) (zip [1..] sol))
+
+main = putStrLn (unlines (formatBoard (nqueens 20) 20))
